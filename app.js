f7e0d76bfaf7cb992592a0deaa8480aa914e3dab
guiyangyang <guiyangyang@playhudong.com>
2018-07-19 16:25:53 +0800

    'vue-笔记-完成'

--- app.js
+++ app.js
@@ -1,11 +1,88 @@
+const Editor = {
+    props: ['entityObject'],
+    data() {
+        return {
+            entity: this.entityObject
+        }
+    },
+    methods: {
+        update() {
+            this.$emit('update')
+        }
+    },
+    template: `
+     <div class="ui form">
+       <div class="field" >
+          <textarea rows='4' placeholder='写点东西...' 
+          v-model='entity.body'
+          @input="update">           
+          </textarea>          
+       </div>
+     </div>   
+`
+}
+
+
 const Note = {
     props: [
       "entityObject"
     ],
+    data() {
+        return {
+            entity: this.entityObject,
+            open: false
+        }
+    },
+    computed: {
+        header() {
+            return _.truncate(this.entity.body, {
+                length: 30
+            })
+        },
+        updated() {
+            return moment(this.entity.meta.updated).fromNow()
+        },
+        words() {
+            return this.entity.body.trim().length
+        }
+    },
+    components: {
+        'editor': Editor
+    },
+    methods: {
+        save() {
+            loadCollection('notes')
+                .then((collection) => {
+                    collection.update(this.entity)
+                    db.saveDatabase()
+                })
+        },
+        destroy() {
+            this.$emit('destroy', this.entity.$loki)
+            console.log('trash')
+        }
+    },
     template: `
    <div class="item">
-     <div calss="content">
-       <div calss="header">笔记</div>
+     <div class="meta">
+        {{updated}}
+     </div>
+     <div class="content">
+       <div class="header" 
+         @click='open = !open'>
+           {{header || '新建笔记'}}
+       </div>
+       <div class="extra">
+       <editor 
+        :entity-object = 'entity'
+        v-if='open'
+        v-on:update='save'>
+       </editor>
+         {{words}}字
+       <i class="right floated trash icon" 
+          v-if='open'
+          v-on:click='destroy'></i>
+       </div>
      </div>
    </div>
 `
@@ -30,6 +107,32 @@ const Notes = {
 
         })
     },
+    methods: {
+        create() {
+            loadCollection('notes').then(collection => {
+                var entity = collection.insert({
+                    'body': ''
+                })
+                db.saveDatabase()
+                this.entities.unshift(entity)
+
+            })
+        },
+        destroy(id) {
+            const _entities = this.entities.filter((entity) => {
+                return entity.$loki !== id
+            })
+            this.entities = _entities //删除数据
+
+            loadCollection('notes')
+                .then((collection) => {
+                    collection.remove({
+                        '$loki': id
+                    })
+                    db.saveDatabase() //数据库删除数据
+                })
+        }
+    },
     components: {
         'note': Note
     },
@@ -39,13 +142,19 @@ const Notes = {
           <li class="paw icon"></li>
           XiaoYang Notes App _ Vue.js
        </h4>
-        <a class="ui right floated basic violet button">添加笔记</a>
+        <a class="ui right floated basic violet button"
+        v-on:click="create">添加笔记</a>
         <div class="ui divided items">
              <note 
                 v-for="entity in entities"
                 v-bind:entityObject = entity
-                v-bind:key = "entity.$loki">
+                v-bind:key = "entity.$loki"
+                v-on:destroy="destroy">
              </note>
+             <span class="ui small disabled header" 
+                 v-if="!this.entities.length"> 
+             还没有笔记，请按下‘添加笔记’按钮！
+             </span>
         </div>
        
      </div>
